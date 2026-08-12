c980b07450367efded41b6169c8aae030ee4ef8c
gabriela martins <gabriela.martins12@aluno.cps.sp.gov.br>
2026-08-11 22:06:41 -0300

    Add files via upload

---
+++ desafio-estrutura.bash/setup-devtech.sh
@@ -0,0 +1,34 @@
+$ mkdir -p devtouch/{projetos/{site-institucional,sistema-interno},financeiro/{2025},>
+> cd devtouch\
+> cd projetos\
+> cd site-institucional\
+> mkdir -p site-institucional/{src{css,js,img},public}\
+> cd public\
+> touch index.html\
+> cd ..\
+> touch README.md\
+> cd ..\
+> cd sistema-interno\
+> mkdir -p sistema-interno/{backend{src},frontend{src}}\
+> cd backend\
+> touch server.js\
+> cd src\
+> mkdir -p src/{controllers,models,routes}\
+> cd ...\
+> cd frontend\
+> touch index.html\
+> cd src\
+> mkdir -p src/{components,pages,services}\
+> cd ....\
+> cd financeiro\
+> cd 2025\
+> touch janeiro.txt\
+> touch fevereiro.txt\
+> touch marco.txt\
+> cd ..\
+> touch relatorio-anual.txt\
+> cd ..\
+> cd rh\
+> touch funcionarios.txt\
+> touch contratacoes-2025.txt\
+
